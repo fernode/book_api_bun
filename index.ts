f59bfab7455731def9102ec04e9cd0d69a0d8723
fernode <fernandosilvadj@hotmail.com>
2023-12-29 00:48:30 -0300

    feat: add logs to applications

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import http from 'http'
+import { logger } from './src/api/config/log/logger'
 import app from './src/app'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 const server = http.createServer(app)
 
-server.listen(port)
+server.listen(port, () =>
+  logger.info({
+    message: `Server listening on port ${port}`,
+  })
+)
