ab7af38bb5ce7b9f1de58cb37d989fbb1c3e1875
fernode <fernandosilvadj@hotmail.com>
2023-12-30 21:56:28 -0300

    refactor: format in eslint style

--- src/api/config/log/logger.ts
+++ src/api/config/log/logger.ts
@@ -1,7 +1,7 @@
 import pinoLogger from 'pino'
 
 export const logger = pinoLogger({
-  level: process.env.PINO_LOG_LEVEL || 'debug',
+  level: process.env.PINO_LOG_LEVEL ?? 'debug',
   transport: {
     target: 'pino-pretty',
     options: {
