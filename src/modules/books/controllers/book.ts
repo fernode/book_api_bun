08fef577a3737ff16008835eedefb676041a3900
fernode <fernandosilvadj@hotmail.com>
2024-01-17 20:30:22 -0300

    refact: Refactor book controller structure

---
+++ src/modules/books/controllers/book.ts
@@ -0,0 +1,56 @@
+import { BookModel } from '@/api/database/schemas'
+import type express from 'express'
+
+export class BookController {
+  public async showAllBooks(
+    req: express.Request,
+    res: express.Response,
+  ): Promise<express.Response> {
+    try {
+      const books = await BookModel.find()
+      return res.status(200).json(books)
+    } catch (error: any) {
+      return res.status(500).send(error.message)
+    }
+  }
+
+  public async create(
+    req: express.Request,
+    res: express.Response,
+  ): Promise<express.Response> {
+    try {
+      console.log(req.body)
+      await BookModel.create(req.body.book)
+      return res.status(200).json({
+        message: 'Book created successfully',
+      })
+    } catch (error: any) {
+      return res.status(500).json({
+        message: 'Failed to create book',
+        error: error.message,
+      })
+    }
+  }
+
+  public async show(
+    req: express.Request,
+    res: express.Response,
+  ): Promise<Express.Response> {
+    try {
+      const { id } = req.params
+      const book = await BookModel.findById(id)
+
+      if (book === null) {
+        return res.status(404).json({
+          message: 'Book not found',
+        })
+      }
+      return res.status(200).json(book)
+    } catch (error: any) {
+      return res.status(500).json({
+        message: 'Failed to find book',
+        error: error.message,
+      })
+    }
+  }
+}
