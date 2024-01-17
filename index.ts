6cba4b867e7a2b41cb8b0e4e6dfb080d3c81c13e
fernode <fernandosilvadj@hotmail.com>
2024-01-17 20:31:06 -0300

    fix: Fix import path and update server.listen syntax

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import http from 'http'
-import { logger } from './src/api/config/log/logger'
+import { logger } from '@/api/config/log/logger.ts'
 import app from './src/app'
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT != null || 8000
 
 const server = http.createServer(app)
 
-server.listen(port, () =>
+server.listen(port, () => {
   logger.info({
     message: `Server listening on port ${port}`,
   })
-)
+})
