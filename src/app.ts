3b2c8b4e27e6594b884dd6e21cb64b8ab72a22b4
fernode <fernandosilvadj@hotmail.com>
2024-01-17 20:26:53 -0300

    refact: add logs for error in db collection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
 import express from 'express'
+import mongoDbConnection from './api/config/database/connection'
+import { logger } from './api/config/log/logger'
 import bookRouter from './api/routes/book'
 const app = express()
+app.use(express.json())
+
+async function databaseConnection(): Promise<void> {
+  try {
+    await mongoDbConnection()
+  } catch (error) {
+    logger.error(error)
+  }
+}
+
+await databaseConnection()
 
 app.use(bookRouter)
 
